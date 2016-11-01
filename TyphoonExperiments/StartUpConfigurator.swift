a3de5ebda1af3db39e0035d473c0d9d9705fb4ee
Alexander Ostrovsky <thealeos@gmail.com>
2016-11-01 22:43:12 +0300

    + StartUpConfigurator

---
+++ TyphoonExperiments/StartUpConfigurator.swift
@@ -0,0 +1,13 @@
+//
+//  StartUpConfigurator.swift
+//  TyphoonExperiments
+//
+//  Created by Alexander Ostrovsky on 01.11.16.
+//  Copyright © 2016 Alexander Ostrovsky. All rights reserved.
+//
+
+import Foundation
+
+class StartUpConfigurator {
+    
+}
